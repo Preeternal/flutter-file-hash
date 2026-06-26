ab1a6474bc27183b4735d16beceb5c67fa3002d3
Preeternal <preeternal@gmail.com>
2026-06-26 22:37:14 +0300

    chore: bootstrap Flutter FFI scaffold

---
+++ example/android/app/src/main/kotlin/com/preeternal/flutter_file_hash/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.preeternal.flutter_file_hash
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test/flutter_file_hash_test.dart
@@ -0,0 +1,13 @@
+import 'package:test/test.dart';
+
+import 'package:flutter_file_hash/flutter_file_hash.dart';
+
+void main() {
+  test('invoke native function', () {
+    expect(sum(24, 18), 42);
+  });
+
+  test('invoke async native callback', () async {
+    expect(await sumAsync(24, 18), 42);
+  });
+}
